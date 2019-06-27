6c392d98defada2a5f22242b024a8e2ac5110b03
Mervksss <49528868+Mervksss@users.noreply.github.com>
2019-06-27 11:44:17 +0300

    e-ticaretSitesi

---
+++ MVCeTicaret/MVCeTicaret/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCeTicaret.Models;
+
+namespace MVCeTicaret.Controllers
+{
+    public class LoginController : Controller
+    {
+        Context db;
+        public LoginController()
+        {
+            db = new Context();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            TemporaryUserData.ReturnUrl = System.Web.HttpContext.Current.Request.UrlReferrer.ToString();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection frm)
+        {
+            string kullaniciAdi = frm["username"];
+            string sifre = frm["password"];
+
+            Customer customer = db.Customers.FirstOrDefault(x => x.UserName == kullaniciAdi && x.Password == sifre);
+
+            if (customer != null)
+            {
+                Session["OnlineKullanici"] = customer.UserName;
+                TemporaryUserData.UserID = customer.CustomerID;
+
+                return Redirect(TemporaryUserData.ReturnUrl);
+            }
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(FormCollection frm)
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCeTicaret/MVCeTicaret/Controllers/ShoppingController.cs
@@ -0,0 +1,77 @@
+﻿using MVCeTicaret.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCeTicaret.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        Context db;
+        public ShoppingController()
+        {
+            db = new Context();
+        }
+        [HttpPost]
+        public ActionResult AddToCart(int id, FormCollection frm)
+        {
+            if (Session["OnlineKullanici"] == null)
+                return RedirectToAction("Login", "Login");
+
+            int miktar = Convert.ToInt32(frm["quantity"]);
+
+            //TODO:CustomerID dinamik hale getirilecek...
+            OrderDetail od = db.OrderDetails.Where(x => x.ProductID == id && x.IsCompleted == false && x.CustomerID==3 ).FirstOrDefault();
+            
+            if (od==null)
+            {
+                od = new OrderDetail();
+                od.CustomerID = 3;//TODO: Dinamik hale getirilecek....
+                od.ProductID = id;
+                od.OrderDate = DateTime.Now;
+                od.IsCompleted = false;
+                od.UnitPrice = db.Products.Find(id).UnitPrice;
+                od.Discount = db.Products.Find(id).Discount;
+
+                if (db.Products.Find(id).UnitInStock >= miktar)
+                    od.Quantity = miktar;
+                else
+                    od.Quantity = db.Products.Find(id).UnitInStock;
+
+                od.TotalAmount = od.Quantity * od.UnitPrice * (1 - od.Discount);
+                db.OrderDetails.Add(od);
+            }
+            else
+            {
+               if(db.Products.Find(id).UnitInStock > od.Quantity + miktar)
+                {
+                    od.Quantity += miktar;
+                    od.TotalAmount = od.Quantity * od.UnitPrice * (1 - od.Discount);
+                }
+            }
+            db.SaveChanges();
+            //TODO: CustomerID dinamik olarak çekilecek...
+            return View(db.OrderDetails.Where(x=>x.CustomerID==3 && x.IsCompleted==false).ToList());
+        }
+
+        public ActionResult AddToWishList(int id)
+        {
+            WishList wl = db.WishLists.FirstOrDefault(x => x.ProductID == id && x.CustomerID == 3 && x.IsActive == true);
+            if (wl == null)
+            {
+                wl = new WishList();
+                wl.CustomerID = 3; //TODO: Dinamik hale getirilecek....
+                wl.ProductID = id;
+                wl.IsActive = true;
+
+                db.WishLists.Add(wl);
+                db.SaveChanges();
+            }
+           
+
+            return View(db.WishLists.Where(x=>x.CustomerID==3 && x.IsActive==true).ToList());
+        }
+    }
+}
